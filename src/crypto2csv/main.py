fb56783a2ad6b627ad1e8c96b8291106176a4a1d
Pratham Agrawal <58286330+prathamagrawal@users.noreply.github.com>
2022-09-05 18:49:59 +0530

    CLI Integration for Detailed Information

--- src/crypto2csv/main.py
+++ src/crypto2csv/main.py
@@ -1,6 +1,11 @@
 import os
 import pandas
+import sys
 
 from utils.detailed_data import detailed_data
 from utils.historical_data import historical_data
 
+if(len(sys.argv)==2):
+    coin=str(sys.argv[1])
+    output=detailed_data(coin)
+    print(output)
